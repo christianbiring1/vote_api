d9817572462ddabb348c83fffcf5ab8de36d8315
christian <christianbiringanine22@gmail.com>
2023-08-28 09:12:26 +0200

    set up the routes for the app

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+
+app.use(express.json());
+
+const candidates = [
+  {
+      id: 1,
+      photo: '',
+      name: 'Williams Doe',
+      position: 'Deputy',
+      political_party: 'UNIC'
+    },
+    {
+      id: 2,
+      photo: '',
+      name: 'Jane Doe',
+      position: 'Deputy',
+      political_party: 'UNIC'
+    },
+    {
+      id: 3,
+      photo: '',
+      name: 'Williams Doe',
+      position: 'Deputy',
+      political_party: 'UNIC'
+    },
+];
 
 app.get('/', (req, res) => {
   res.send('Welcome to Vote App!');
 });
 
+app.get('/api/elections', (req, res) => {
+  res.send('votes')
+
+});
+
+app.get('/api/elections/:id', (req, res) => {
+  res.send(req.params.id)
+  
+});
+
+app.get('/api/candidates', (req, res) => {
+  res.send(candidates)
+  
+});
+
+app.get('/api/candidates/:id', (req, res) => {
+  const candidate = candidates.find(c => c.id === parseInt(req.params.id));
+  console.log(candidate)
+  if(!candidate) res.status(404).send("This candidates with the given ID was not found")
+  res.send(candidate);
+})
+
+
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
