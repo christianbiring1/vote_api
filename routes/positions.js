47ef7d53815e58971c4d1373e3a4b746aef910fc
christian <christianbiringanine22@gmail.com>
2023-09-01 18:51:45 +0200

    Protecting the routes handler post of position to make sure only auth user admin can modify that

--- routes/positions.js
+++ routes/positions.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const {Position, validatePosition} = require('../models/position');
+const auth = require('../middleware/auth');
 
 const express = require('express');
 const router = express.Router();
@@ -9,7 +10,7 @@ router.get('/', async (req, res) => {
   res.send(positions);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const { error } = validatePosition(req.body);
   if(error) return res.status(400).send(error.details[0].message);
 
