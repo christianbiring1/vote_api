e190db26c5a214972de5c448b277802211d28729
christian <christianbiringanine22@gmail.com>
2023-09-01 17:29:00 +0200

    Hash the user password to store it in the db

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
+
 const {User, validateUser} = require('../models/user');
 
 const express = require('express');
@@ -13,6 +15,8 @@ router.post('/', async (req, res) => {
   if(user) return res.status(400).send('User already registered.');
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
 
