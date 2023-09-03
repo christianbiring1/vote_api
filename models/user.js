4580554cc1b12a81a93ea8100014d3274691d42b
christian <christianbiringanine22@gmail.com>
2023-09-04 00:56:32 +0200

    Add the name and email in the jwt

--- models/user.js
+++ models/user.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function() {
-  const token = jwt.sign({ _id: this._id }, 'jwtPrivateKey');
+  const token = jwt.sign({ _id: this._id, name: this.name, email: this.email }, 'jwtPrivateKey');
   return token;
 };
 
