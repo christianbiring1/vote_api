52fc2f27390609b120a6895d6f7e5646364447c5
christian <christianbiringanine22@gmail.com>
2023-09-27 13:43:42 +0200

    Add the vote increasement in a transaction

--- routes/votes.js
+++ routes/votes.js
@@ -24,27 +24,45 @@ router.post('/', async (req, res) => {
   // let vote = await Vote.findOne(_.pick(req.body, ['candidate', 'elector']));
   // if(vote) return res.status(403).send('You cannot vote more than once!')
 
+  const session = await mongoose.startSession();
+  session.startTransaction();
 
-  const vote = new Vote({
-    candidate: {
-      _id: candidate._id,
-      first_name: candidate.first_name,
-      last_name: candidate.last_name,
-      position: candidate.position,
-      political_party: candidate.political_party
-    },
-    elector: {
-      _id: elector._id,
-      name: elector.name,
-      id: elector.id,
-      province: elector.province
-    }
-  });
+  try {
+    // Create a new vote
+    const vote = new Vote({
+      candidate: {
+        _id: candidate._id,
+        first_name: candidate.first_name,
+        last_name: candidate.last_name,
+        position: candidate.position,
+        political_party: candidate.political_party
+      },
+      elector: {
+        _id: elector._id,
+        name: elector.name,
+        id: elector.id,
+        province: elector.province
+      }
+    });
 
-  candidate.voice += 1;
-  candidate.save();
-  await vote.save();
-  res.send(vote);
+    // Increase the candidate's vote count
+
+    candidate.voice += 1;
+
+    // Save both the vote and the updated candidate inside the transaction
+    await vote.save({ session });
+    await candidate.save({ session });
+
+    await session.commitTransaction();
+    session.endSession();
+
+
+    res.send(vote);
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    res.status(500).send('Transaction failed. Vote not recorded.');
+  }
 });
 
 module.exports = router;
